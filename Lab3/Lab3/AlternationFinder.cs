13995e60b2a84424ac95468051d2c8c6e0a22e4b
Rise <ritm-temp@hotmail.com>
2018-02-27 10:44:54 +0600

    Lab3v0.5

---
+++ Lab3/Lab3/AlternationFinder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class AlternationFinder
+    {
+        public string IsItAlternate(int[] massiveOfNumbers)
+        {
+            string[] result;
+            int alternate = 0;
+            int previousNumber = 0;
+            int currentNumber = 0;
+            int NumberOfLastNumber=0;
+            for(int i = 0; i < massiveOfNumbers.Length; i++)
+            {
+                if (massiveOfNumbers[i] % 2 == 0)
+                {
+                    currentNumber = 1;
+                }
+                if (previousNumber != currentNumber)
+                {
+                    alternate = 1;
+                }
+                else
+                {
+                    alternate = 0;
+                    NumberOfLastNumber = i;
+                }
+            }
+            result[1] = NumberOfLastNumber.ToString();
+            return result;
+        }
+    }
+}
---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] massiveOfNumber = {1,2,3,4,5,6,7,8,9,10 };
+
+        }
+    }
+}
