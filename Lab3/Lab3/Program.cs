8a5555fa305af7fc7cc76aa335aba6989bc1d577
Rise <ritm-temp@hotmail.com>
2018-03-06 10:09:52 +0600

    Lab3 v1.01

--- Lab3/Lab3/Program.cs
+++ Lab3/Lab3/Program.cs
@@ -9,7 +9,7 @@ namespace Lab3
             int[] massiveOfNumber = {1,2,3,4,5,6,7,8,9,10 };
             AlternationFinder newFinder = new AlternationFinder();
             String alternate = newFinder.IsItAlternate(massiveOfNumber);
-            Console.WriteLine($"{alternate}");
+            Console.WriteLine($"Номер первого элемента, нарущающего очередность( 0 означает, что очередность не нарушена): {alternate}");
             Console.ReadLine();
         }
     }
